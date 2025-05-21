bcacc344ab68e9fe3b2fb095e20d5f078a15a8fd
KANG-XUAN <kang900320@gmail.com>
2025-05-21 16:36:45 +0800

    註冊完成！！！！！！！！

--- src/components/Register/RegisterStep4.vue
+++ src/components/Register/RegisterStep4.vue
@@ -1,19 +1,28 @@
 <template>
-	<!-- Step 4 -->
-	<div :class="['step', nowStep === 4 ? 'active-step' : '']">
-		<h4>【肆．確認您的資料並驗證】</h4>
+  <!-- Step 4 -->
+  <div id="step4" :class="['step', nowStep === 4 ? 'active-step' : '']">
+    <h4>【肆．確認您的資料並驗證】</h4>
 
-		<p>暱稱：{{ form.nickname }}</p>
-		<p>性別：{{ genderText }}</p>
-		<p>生日：{{ form.birth }}</p>
-		<p>電子郵件：{{ form.email }}</p>
-		<p>電話：{{ form.phone }}</p>
-	</div>
+    <h5>{{ form.nickname }}</h5>
+    <div class="underline"></div>
+
+    <span class="name">性別：</span>
+    <span class="data">{{ genderText }}</span>
+    <br>
+    <span class="name">生日：</span>
+    <span class="data">{{ form.birth }}</span>
+    <br>
+    <span class="name">電子郵件：</span>
+    <span class="data">{{ form.email }}</span>
+    <br>
+    <span class="name">電話：</span>
+    <span class="data">{{ form.phone }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "RegisterStep4",
+  name: "RegisterStep4",
   props: {
     nowStep: Number,
     form: {
@@ -21,16 +30,66 @@ export default {
       required: true
     }
   },
+  // 本地資料庫
+  data() {
+    return {
+      localForm: {
+        nickname: this.form.nickname,
+        gender: this.form.gender,
+        birth: this.form.birth,
+        email: this.form.email,
+        phone: this.form.phone,
+      },  // 複製 prop 避免直接改
+    };
+  },
   computed: {
-  genderText() {
-    switch (this.form.gender) {
-      case 'male': return '男';
-      case 'female': return '女';
-      case 'hidden': return '隱藏';
-      default: return '未選擇';
+    genderText() {
+      switch (this.form.gender) {
+        case 'male': return '男';
+        case 'female': return '女';
+        case 'hidden': return '隱藏';
+        default: return '未選擇';
+      }
+    }
+  },
+  methods: {
+    validateForm() {
+      // 增加驗證碼判斷
+
+      return true;
     }
   }
 }
-}
 </script>
 
+<style>
+#step4 {
+
+  /* 暱稱 */
+  h5 {
+    font-size: 24px;
+    text-align: center;
+    margin-bottom: 0;
+  }
+
+  /* 底線 */
+  .underline {
+    width: 70%;
+    height: 1px;
+    background-color: black;
+    margin: 10px auto;
+  }
+
+  /* 文字 */
+  .name {
+    display: inline-block;
+    color: var(--main-color);
+    width: 80px;
+  }
+
+  /* 資料 */
+  .data {
+    margin-left: 10px;
+  }
+}
+</style>
